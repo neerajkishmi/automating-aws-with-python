abcb711afc9a585070d95a113a600fa058a4e60a
Neeraj Kishmi <neeraj.kishmi@gmail.com>
2020-07-11 21:47:22 -0600

    Output website host when syncing

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -20,6 +20,7 @@ from bucket import BucketManager
 session = None
 bucket_manager = None
 
+
 @click.group()
 @click.option('--profile', default=None,
               help="Use a given AWS profile.")
@@ -27,7 +28,7 @@ def cli(profile):
     """Webotron deploys websites to AWS."""
     global session, bucket_manager
 
-    session_cfg={}
+    session_cfg = {}
     if profile:
         session_cfg['profile_name'] = profile
 
@@ -66,6 +67,7 @@ def setup_bucket(bucket):
 def sync(pathname, bucket):
     """Sync contents of PATHNAME to BUCKET."""
     bucket_manager.sync(pathname, bucket)
+    print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
 
 
 if __name__ == '__main__':
